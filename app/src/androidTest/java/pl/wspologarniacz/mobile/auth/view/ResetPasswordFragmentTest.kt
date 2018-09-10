d479554de0040a14143b2bf8cde461ff34dc58a5
p.adamczyk <p.adamczyk@partner.samsung.com>
2018-09-10 16:30:37 +0200

    add testing to reset password

--- app/src/androidTest/java/pl/wspologarniacz/mobile/auth/view/ResetPasswordFragmentTest.kt
+++ app/src/androidTest/java/pl/wspologarniacz/mobile/auth/view/ResetPasswordFragmentTest.kt
@@ -3,6 +3,7 @@ package pl.wspologarniacz.mobile.auth.view
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -41,6 +42,24 @@ class ResetPasswordFragmentTest {
 
         //clean up
         openResetPasswordFromRegisterFragment()
+    }
+
+    @Test
+    fun testNavigationToLoginFragment() {
+        Navigation.findNavController(activityRule.activity, R.id.nav_host_fragment).navigateUp()
+
+        onView(withId(R.id.header)).check(matches(ViewMatchers.withText(R.string.sign_in_text)))
+        onView(withId(R.id.action)).check(matches(ViewMatchers.withText(R.string.sign_in_text)))
+
+        onView(withId(R.id.confirmPassword)).check(ViewAssertions.doesNotExist())
+        onView(withId(R.id.username)).check(ViewAssertions.doesNotExist())
+        onView(withId(R.id.reset_password)).check(matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.password)).check(matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.email)).check(matches(ViewMatchers.isDisplayed()))
+
+        //clean up
+        Navigation.findNavController(activityRule.activity, R.id.nav_host_fragment).navigate(R.id.action_loginFragment_to_resetPasswordFragment)
+
     }
 
     private fun openResetPasswordFromRegisterFragment() {
