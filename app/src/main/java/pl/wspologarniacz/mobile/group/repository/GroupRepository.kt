fa3b658b060b999502b3bccebb7e9d905deadb09
p.adamczyk <p.adamczyk@partner.samsung.com>
2018-09-26 16:54:45 +0200

    add base group object and group methods to repository

--- app/src/main/java/pl/wspologarniacz/mobile/group/repository/GroupRepository.kt
+++ app/src/main/java/pl/wspologarniacz/mobile/group/repository/GroupRepository.kt
@@ -3,6 +3,7 @@ package pl.wspologarniacz.mobile.group.repository
 import androidx.lifecycle.MutableLiveData
 import pl.wspologarniacz.mobile.common.repository.model.Idle
 import pl.wspologarniacz.mobile.common.repository.model.LoadState
+import pl.wspologarniacz.mobile.group.repository.model.Group
 
 class GroupRepository {
 
@@ -11,20 +12,47 @@ class GroupRepository {
         value = Idle()
     }
 
-
-    fun search(query: String) {
-
-    }
-
-    fun add() {
+    fun acceptInvitation(groupId: Long) {
 
     }
 
-    fun join(groupId: Long) {
+    fun rejectInvitation(groupId: Long) {
 
     }
 
-    fun getList() {
+    fun acceptUser(groupId: Long, userId: Long) {
+
+    }
+
+    fun rejectUser(groupId: Long, userId: Long) {
+
+    }
+
+    fun invite(groupId: Long, userId: Long) {
+
+    }
+
+    fun seachGroup(query: String) {
+
+    }
+
+    fun getGroup(groupId: Long) {
+
+    }
+
+    fun getUserGroups() {
+
+    }
+
+    fun joinGroup(groupId: Long) {
+
+    }
+
+    fun leaveGroup(groupId: Long) {
+
+    }
+
+    fun createGroup(group: Group) {
 
     }
 }
---
+++ app/src/main/java/pl/wspologarniacz/mobile/group/repository/model/Group.kt
@@ -0,0 +1,5 @@
+package pl.wspologarniacz.mobile.group.repository.model
+
+data class Group(var id: Long,
+                 var name: String) {
+}
