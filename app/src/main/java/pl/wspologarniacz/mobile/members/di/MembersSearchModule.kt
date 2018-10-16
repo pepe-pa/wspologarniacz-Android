5bbfa96ce5657490309fbd4b85ef1768d2775c7e
p.adamczyk <p.adamczyk@partner.samsung.com>
2018-10-16 14:14:17 +0200

    fix build error

--- app/src/main/java/pl/wspologarniacz/mobile/members/di/MembersSearchModule.kt
+++ app/src/main/java/pl/wspologarniacz/mobile/members/di/MembersSearchModule.kt
@@ -4,13 +4,14 @@ import dagger.Module
 import dagger.Provides
 import pl.wspologarniacz.mobile.common.di.GroupModule
 import pl.wspologarniacz.mobile.common.viewmodel.ViewModelFactory
+import pl.wspologarniacz.mobile.group.repository.GroupRepository
 import pl.wspologarniacz.mobile.members.viewmodel.MembersSearchViewModel
 
 @Module(includes = [GroupModule::class])
 class MembersSearchModule {
 
     @Provides
-    fun viewModel() = MembersSearchViewModel()
+    fun viewModel(groupRepository: GroupRepository) = MembersSearchViewModel(groupRepository)
 
     @Provides
     fun viewModelFactory(viewModel: MembersSearchViewModel) = ViewModelFactory(viewModel)
